9a603ad20ac50effa3e66b656912729833010445
masudulalam <masuudulalam@gmail.com>
2025-01-20 15:02:03 +0600

    explore navigate onClick

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Post = ({ post }) => {
     const {id, title} = post;
+    const navigate = useNavigate();
+
     const postStyles = {
         border: "2px solid mediumseagreen",
         padding: "10px",
@@ -10,12 +12,18 @@ const Post = ({ post }) => {
         marginBottom: "10px",
       };
 
+      const handleShowDetails = () => {
+        navigate(`/post/${id}`)
+      }
+
     return (
         <div style={postStyles}>
             <h4>Post of Id: {id}</h4>
             <p>{title}</p>
             <Link to={`/post/${id}`}>Post Details</Link>
             <Link to={`/post/${id}`}><button>Post Details</button></Link>
+            <br />
+            <button onClick={handleShowDetails}>Click to See Details</button>
         </div>
     );
 };
